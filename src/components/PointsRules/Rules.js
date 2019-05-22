5d1bd915f2582e8aa080c7431a2c57016c5c7f84
Glazinski <vercety.glaz@gmail.com>
2019-05-22 11:12:27 +0200

    Add Rules component

---
+++ src/components/PointsRules/Rules.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import H2 from '../H2/H2.js';
+
+const StyledSection = styled.section`
+    height: auto;
+`;
+
+const ClipPath = styled.div`
+
+`;
+
+const Rules = (props) => {
+    return (
+        <>
+        </>
+    );
+}
+
+export default Rules;
