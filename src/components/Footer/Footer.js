92c97124f066bd6de1336877b8f625668a9dff97
Glazinski <vercety.glaz@gmail.com>
2019-06-09 15:44:47 +0200

    Change form in footer to static height

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,7 +11,7 @@ const ClipPath = styled.div`
     
     clip-path: polygon(0 22%, 100% 37%, 100% 100%, 0% 100%);
     background: ${({theme}) => theme.colors.primary};
-    height: 100%;
+    height: 670px;
     width: 100%;
 
     position: absolute;
@@ -25,7 +25,7 @@ const ContactForm = styled.form`
     align-items: center;
     flex-direction: column;
 
-    height: 80%;
+    height: 500px;
     width: 80%;
     background-color: ${({theme}) => theme.colors.white};
     border: 1px solid ${({theme}) => theme.colors.lightBlack};
