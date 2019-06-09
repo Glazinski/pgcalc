56adf60161a06b0f6d5e00e6bd6b62c1ffaa5861
Glazinski <vercety.glaz@gmail.com>
2019-06-09 20:11:11 +0200

    Add hidden input

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -114,6 +114,7 @@ const Footer = (props) => {
                 netlify
                 data-netlify-honeypot="bot-field"
             >
+                <input type="hidden" name="form-name" value="contact" />
                 <StyledInput
                     name="name"
                     type="email"
