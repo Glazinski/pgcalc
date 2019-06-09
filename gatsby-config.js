7e70dfa5071e1adce4bb431d4bf8d15d8aa3f71e
Glazinski <vercety.glaz@gmail.com>
2019-06-09 15:07:23 +0200

    Change config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `pgkalkulator`,
+    description: `A project that calculates your points based on your
+    mature exams results.`,
+    author: `Michał Kamiński`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
